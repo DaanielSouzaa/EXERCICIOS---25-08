6c6543acf12359a631d529c9387db0c587885f33
DaanielSouzaa <danielavelino@ucl.br>
2020-08-26 16:49:38 +0000

    EX_2

--- main.cs
+++ main.cs
@@ -1,57 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-class MainClass {
+public class Example
+{ 
+  string resposta = "s";
+  string item;
 
-  public static double mult (int num1, int num2) {
-    return num1 * num2;
-  }
-  public static int adicao (int num1,int num2) {
-    return num1 + num2;
-  }
-  public static int subtracao (int num1,int num2) {
-    return num1 + num2;
-  }
-  public static double divisao (int num1,int num2) {
-    return ((double)num1 / num2);
-  }
+    public static void GeraCardapio() {
+      IDictionary<string, string> ItemNome =
+            new Dictionary<string, string>();
 
-  public static void calc () {
+        ItemNome.Add("100", "Cachorro quente");
+        ItemNome.Add("101", "Bauru Simples");
+        ItemNome.Add("102", "Bauru com ovo");
+        ItemNome.Add("103", "Hamburguer");
+        ItemNome.Add("104", "Cheeseburguer");
+        ItemNome.Add("105", "Refrigerante");
+    
+      IDictionary<string, double> ItemValor =
+              new Dictionary<string, double>();
 
-  int num1 = 0;
-  int num2 = 0;
-  string operador;
-  double resultado = 0;
+          ItemValor.Add("100",1.70);
+          ItemValor.Add("101",2.30);
+          ItemValor.Add("102",2.60);
+          ItemValor.Add("103",2.40);
+          ItemValor.Add("104",2.50);
+          ItemValor.Add("105",1.00);
+    }
 
-    Console.WriteLine("Digite um número inteiro");
-    num1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Digite um número inteiro");
-    num2 = int.Parse(Console.ReadLine());
+    public static void ExibeCardapio(ItemNome,ItemValor) {
+        foreach(KeyValuePair<string, string> kvp in ItemNome)
+        {
+            Console.WriteLine("{0} {1} {2}",
+                kvp.Key, kvp.Value,ItemValor[kvp.key]);
+        }
+    }
 
-    Console.WriteLine("Que operação deseja realizar?");
-    operador = Console.ReadLine();
-
-    switch (operador) {
-      case "/":
-        resultado = divisao(num1,num2);
-        break;
-      case "*":
-        resultado = mult(num1,num2);
-        break;
-      case "+":
-        resultado = adicao(num1,num2);
-        break;
-      case "-":
-        resultado = subtracao(num1,num2);
-        break;
-      default:
-        Console.WriteLine("Opção inválida!");
-        break;
-    };
-
-    Console.WriteLine("O resultado é:" + resultado);
-  }
-
-  public static void Main () {
-    calc();
-  }
+    public static void Main()
+    {
+      GeraCardapio();
+      ExibeCardapio(ItemNome);
+    }
 }
